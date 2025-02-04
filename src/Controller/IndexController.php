4ffb8fac62dbccf37d621b9ffb4f8efa9178ec7f
Gaël Reyrol <me@gaelreyrol.dev>
2025-02-04 17:46:44 +0100

    Configuration locale de l'environnement Kubernetes (#21)
    feat: create helm chart and setup skaffold

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adictiz\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('Hello World!', 200, ['Content-Type' => 'text/plain']);
+    }
+
+    #[Route('/exception')]
+    public function exception(): Response
+    {
+        throw new BadRequestHttpException('Oups!');
+    }
+}
